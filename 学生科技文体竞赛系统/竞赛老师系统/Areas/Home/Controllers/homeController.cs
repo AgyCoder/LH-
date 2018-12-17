acb61fb438f15bf930d946f412864388177c3665
黎依龙 <347806896@qq.com>
2018-12-17 23:55:57 +0800

    test

---
+++ 学生科技文体竞赛系统/竞赛老师系统/Areas/Home/Controllers/homeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace 竞赛老师系统.Areas.Home.Controllers
+{
+    public class homeController : Controller
+    {
+        // GET: Home/home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
