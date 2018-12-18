20dc17cafcee4dcc635f49d76796c0835b3eec00
黎依龙 <347806896@qq.com>
2018-12-18 13:43:45 +0800

    老师后台前端修改

---
+++ 学生科技文体竞赛系统/竞赛老师系统/Areas/TeacherInfo/Controllers/TeacherInfoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace 竞赛老师系统.Areas.TeacherInfo.Controllers
+{
+    public class TeacherInfoController : Controller
+    {
+        // GET: TeacherInfo/TeacherInfo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ 学生科技文体竞赛系统/竞赛老师系统/Areas/TeacherInfo/TeacherInfoAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace 竞赛老师系统.Areas.TeacherInfo
+{
+    public class TeacherInfoAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "TeacherInfo";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "TeacherInfo_default",
+                "TeacherInfo/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
