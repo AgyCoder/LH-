a2005c74ae749a3add11e508194df2d84efdac5d
黎依龙 <347806896@qq.com>
2018-12-24 02:14:35 +0800

    更改退回页面

---
+++ 学生科技文体竞赛系统/竞赛老师系统/Areas/fabu/Controllers/IndexController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace 竞赛老师系统.Areas.fabu.Controllers
+{
+    public class IndexController : Controller
+    {
+        private StudentsCompetitionSystemEntities db = new StudentsCompetitionSystemEntities();
+        // GET: fabu/Index
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult Preservation(string project, string category, string content,string date)
+        {
+
+
+            SCS_Competition scS_Competition = new SCS_Competition
+            {
+                竞赛名称 = project,
+                类别 = category,
+                截止时间 = date,
+                内容 = content,
+                发布时间 = DateTime.Now.ToString()
+            };
+            int user_Login = Convert.ToInt32(Session["username"]);
+            scS_Competition.职工号 =  user_Login;
+
+            db.SCS_Competition.Add(scS_Competition);
+            db.SaveChanges();
+
+            return Json(true);
+        }
+    }
+}
---
+++ 学生科技文体竞赛系统/竞赛老师系统/Areas/fabu/fabuAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace 竞赛老师系统.Areas.fabu
+{
+    public class fabuAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "fabu";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "fabu_default",
+                "fabu/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
