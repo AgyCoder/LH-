e71575e7a8da40247ee1ac483cecb8fd9c6a7d13
黎依龙 <347806896@qq.com>
2018-12-24 20:40:08 +0800

    更改

---
+++ 学生科技文体竞赛系统/竞赛老师系统/Areas/TeacherInfo/Controllers/SCS_TeacherController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using 竞赛老师系统;
+
+namespace 竞赛老师系统.Areas.TeacherInfo.Controllers
+{
+    public class SCS_TeacherController : Controller
+    {
+        private StudentsCompetitionSystemEntities db = new StudentsCompetitionSystemEntities();
+
+        // GET: TeacherInfo/SCS_Teacher
+        public ActionResult Index()
+        {
+            return View(db.SCS_Teacher.ToList());
+        }
+
+        // GET: TeacherInfo/SCS_Teacher/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SCS_Teacher sCS_Teacher = db.SCS_Teacher.Find(id);
+            if (sCS_Teacher == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sCS_Teacher);
+        }
+
+        // GET: TeacherInfo/SCS_Teacher/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TeacherInfo/SCS_Teacher/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,姓名,职工号,手机号码,邮箱,办公地址")] SCS_Teacher sCS_Teacher)
+        {
+            if (ModelState.IsValid)
+            {
+                db.SCS_Teacher.Add(sCS_Teacher);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(sCS_Teacher);
+        }
+
+        // GET: TeacherInfo/SCS_Teacher/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SCS_Teacher sCS_Teacher = db.SCS_Teacher.Find(id);
+            if (sCS_Teacher == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sCS_Teacher);
+        }
+
+        // POST: TeacherInfo/SCS_Teacher/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,姓名,职工号,手机号码,邮箱,办公地址")] SCS_Teacher sCS_Teacher)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sCS_Teacher).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(sCS_Teacher);
+        }
+
+        // GET: TeacherInfo/SCS_Teacher/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SCS_Teacher sCS_Teacher = db.SCS_Teacher.Find(id);
+            if (sCS_Teacher == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sCS_Teacher);
+        }
+
+        // POST: TeacherInfo/SCS_Teacher/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            SCS_Teacher sCS_Teacher = db.SCS_Teacher.Find(id);
+            db.SCS_Teacher.Remove(sCS_Teacher);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        
+
+    }
+}
--- 学生科技文体竞赛系统/竞赛老师系统/Areas/TeacherInfo/Controllers/TeacherInfoController.cs
+++ 学生科技文体竞赛系统/竞赛老师系统/Areas/TeacherInfo/Controllers/TeacherInfoController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,11 +9,60 @@ namespace 竞赛老师系统.Areas.TeacherInfo.Controllers
 {
     public class TeacherInfoController : Controller
     {
+        private static StudentsCompetitionSystemEntities db = new StudentsCompetitionSystemEntities();
         // GET: TeacherInfo/TeacherInfo
         public ActionResult Index()
         {
             return View();
         }
 
+        public static SCS_Teacher Get_TeacherInfo(int id)
+        {
+            try
+            {
+                
+                SCS_Teacher sCS_Teacher = new SCS_Teacher();
+                var result1 = db.SCS_Teacher.Where(c => c.职工号.Equals(id)).Distinct();
+                if (result1.Count() > 0)
+                {
+                    foreach (var r in result1)
+                    {
+                        //view_Userinfo.驾驶证号码 = r.驾驶证号码;
+                        sCS_Teacher.职工号 = r.职工号;
+                        sCS_Teacher.姓名 = r.姓名;
+                        sCS_Teacher.手机号码 = r.手机号码;
+                        sCS_Teacher.邮箱 = r.邮箱;
+                        sCS_Teacher.办公地址 = r.办公地址;
+                    }
+                }
+                
+                return sCS_Teacher;
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        public JsonResult GetMessage()
+        {
+            SCS_Teacher user = new SCS_Teacher();
+
+           return Json(TeacherInfoController.Get_TeacherInfo(Convert.ToInt32(Session["id"])));
+        }
+
+        public JsonResult Revise(string name,int tel,string Email ,string Office)
+        {
+
+            SCS_Teacher sCS_Teacher = db.SCS_Teacher.Where(c => c.职工号.Equals(Convert.ToInt32(Session["id"]))).FirstOrDefault();
+            sCS_Teacher.姓名 = name;
+            sCS_Teacher.手机号码 = tel;
+            sCS_Teacher.邮箱 = Email;
+            sCS_Teacher.办公地址 = Office;
+            db.SCS_Teacher.Add(sCS_Teacher);
+            db.SaveChanges();
+
+            return Json(true);
+        }
     }
 }
